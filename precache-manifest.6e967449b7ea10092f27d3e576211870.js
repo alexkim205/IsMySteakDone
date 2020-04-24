b19873651be7ce16bd8aea9fd1e31440a76e4743
Alex Gyujin Kim <alexgkim205@gmail.com>
2020-04-24 12:49:28 -0400

    Updates

---
+++ precache-manifest.6e967449b7ea10092f27d3e576211870.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7e9b99e3bf4cd3094c499533078489",
+    "url": "/IsMySteakDone/index.html"
+  },
+  {
+    "revision": "222abb9a233147da1b85",
+    "url": "/IsMySteakDone/static/css/2.c8cfea30.chunk.css"
+  },
+  {
+    "revision": "fae8b8129d47772ec195",
+    "url": "/IsMySteakDone/static/css/main.58abf7bd.chunk.css"
+  },
+  {
+    "revision": "222abb9a233147da1b85",
+    "url": "/IsMySteakDone/static/js/2.df2f62db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/IsMySteakDone/static/js/2.df2f62db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae8b8129d47772ec195",
+    "url": "/IsMySteakDone/static/js/main.07e8f690.chunk.js"
+  },
+  {
+    "revision": "1c14fa26a6d8bb4caae7",
+    "url": "/IsMySteakDone/static/js/runtime-main.f4a4fbc1.js"
+  },
+  {
+    "revision": "c0da8da71a6ec4b3b1d6d227ca30a52d",
+    "url": "/IsMySteakDone/static/media/face.c0da8da7.png"
+  },
+  {
+    "revision": "3575ce4c4008f03c07b40ba60fd818b6",
+    "url": "/IsMySteakDone/static/media/fist.3575ce4c.png"
+  },
+  {
+    "revision": "dbf8edca7b173d44c6240603e1cc67d8",
+    "url": "/IsMySteakDone/static/media/palm-medium-rare.dbf8edca.png"
+  },
+  {
+    "revision": "c4f11b87626cf10d6f88bc6c00301c2c",
+    "url": "/IsMySteakDone/static/media/palm-medium.c4f11b87.png"
+  },
+  {
+    "revision": "59a59359c6901a12c703dd7e48951831",
+    "url": "/IsMySteakDone/static/media/palm-rare.59a59359.png"
+  },
+  {
+    "revision": "a236fd26105710e39c9a69855f947a0d",
+    "url": "/IsMySteakDone/static/media/palm-well-done.a236fd26.png"
+  },
+  {
+    "revision": "cf2bd066d453c00320906978912f919c",
+    "url": "/IsMySteakDone/static/media/palm.cf2bd066.png"
+  },
+  {
+    "revision": "16b955f006222e70ab0651cb78959e83",
+    "url": "/IsMySteakDone/static/media/steak-medium-rare.16b955f0.jpg"
+  },
+  {
+    "revision": "083b512fbae39da7a6540785fbc94997",
+    "url": "/IsMySteakDone/static/media/steak-medium.083b512f.jpg"
+  },
+  {
+    "revision": "f0c6f7866d4d320544f1972238767be1",
+    "url": "/IsMySteakDone/static/media/steak-rare.f0c6f786.png"
+  },
+  {
+    "revision": "838d63ff79f8e623bc31c42ef35d4ae2",
+    "url": "/IsMySteakDone/static/media/steak-well-done.838d63ff.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d0db7ad42f9cae2163a6d4d73b20404.js"
+  "/IsMySteakDone/precache-manifest.6e967449b7ea10092f27d3e576211870.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IsMySteakDone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
